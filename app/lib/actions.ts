b7d010815f57817e73c90e52262e74031dd86b41
aprietof <adrianprieto00@gmail.com>
2024-03-06 22:44:55 -0500

    Remove error example

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -82,8 +82,6 @@ export async function updateInvoice(id: string, formData: FormData) {
 
 // DELETE
 export async function deleteInvoice(id: string) {
-  throw new Error('Failed to Delete Invoice');
-
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
